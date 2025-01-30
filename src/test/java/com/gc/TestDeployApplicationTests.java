efbc042a95e45b72659048e2cf937bb0df5f5d3d
Santosh Mahto <santoshkumar.mahto@telusinternational.com>
2025-01-30 15:11:55 +0530

    new version file add 4

--- src/test/java/com/gc/TestDeployApplicationTests.java
+++ src/test/java/com/gc/TestDeployApplicationTests.java
@@ -1,13 +1,12 @@
-package com.gc;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TestDeployApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.gc;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class TestDeployApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
